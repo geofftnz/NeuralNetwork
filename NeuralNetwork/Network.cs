c864d0421499ae4ff934aad957bd790ad64965d1
Geoff Thornburrow <geofftnz@gmail.com>
2021-09-05 10:18:07 +1200

    initial

---
+++ NeuralNetwork/Network.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NeuralNetwork
+{
+    /*
+    Simple, oldskool backprop Neural Network.
+
+    What we need:
+    - Node activations (big array of floats) - inputs given to network initially, remainder updated when network runs. Probably separate this out.
+    - Node input weights (big array of floats)
+    - Node input weight derivatives
+
+     */
+    public class Network
+    {
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+A port of some 20yo neural net code, based on a 30yo book (PDP1 by David E. Rumelhart, James L. McClelland and PDP Research Group)
